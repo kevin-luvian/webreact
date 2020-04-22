98f6436a7bb138aa7412811ccde83c8e59c0e22c
kevin-luvian <kevinlh90@gmail.com>
2020-04-22 07:12:17 +0700

    refactor: delete unused files

--- frontend/post_build.ps1
+++
@@ -1,4 +0,0 @@
-    //"postbuild": "@powershell -NoProfile -ExecutionPolicy Unrestricted -Command ./post_build.ps1",
-
-Copy-Item "./build/*" -Destination "../src/main/resources/templates" -Recurse -force
-Copy-Item "./build/assets/**" -Destination "../src/main/resources/static/assets" -Recurse -force
