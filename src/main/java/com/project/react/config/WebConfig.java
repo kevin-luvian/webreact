cbceec965470a4b9ce311652ab625bdc433f522b
kevin-luvian <kevinlh90@gmail.com>
2020-05-14 16:11:56 +0700

    refactor: change url

--- src/main/java/com/project/react/config/WebConfig.java
+++ src/main/java/com/project/react/config/WebConfig.java
@@ -7,13 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-	  registry.addViewController("/{spring:[\S]*}")
-            .setViewName("forward:/");
-      registry.addViewController("/**/{spring:[\S]*}")
-            .setViewName("forward:/");
-      registry.addViewController("/{spring:[\S]*}/**{spring:?!(\\.js|\\.css)$}")
-            .setViewName("forward:/");
-  }
+      @Override
+      public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addViewController("/{spring:[a-zA-Z0-9-_]+}").setViewName("forward:/");
+            registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/");
+            registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/");
+      }
 }
